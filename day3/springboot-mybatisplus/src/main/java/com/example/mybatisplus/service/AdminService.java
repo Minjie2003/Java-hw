f25d9b62b21460486a10208b1dbe7cd39d3a3466
Minjie2003 <1276679294@qq.com>
2024-06-23 17:40:10 +0800

    update day3

---
+++ day3/springboot-mybatisplus/src/main/java/com/example/mybatisplus/service/AdminService.java
@@ -0,0 +1,18 @@
+package com.example.mybatisplus.service;
+
+import com.example.mybatisplus.model.domain.Admin;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lxp
+ * @since 2021-06-17
+ */
+public interface AdminService extends IService<Admin> {
+
+}
